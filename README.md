ad3d738a5c59a40e3dac03d73a020d4ca573e733
Padmanabha Das <padmanabhadas9647@gmail.com>
2024-08-27 11:40:59 +0530

    project setup in progress

---
+++ README.md
@@ -0,0 +1,11 @@
+# Uber
+
+# Dependencies
+
+### Tailwind CSS - https://www.nativewind.dev/quick-starts/expo
+    npm install nativewind
+    npm install --save-dev tailwindcss@3.3.2
+    npx tailwindcss init (Add folders inside tailwind.config.js > content)
+    Add plugins array in babel.config.js (plugins: ["nativewind/babel"],)
+
+# Preview
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import {Text} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+function HomeScreen() {
+  return (
+      <SafeAreaView>
+          <Text className={'text-2xl font-black text-red-400'}>Hello Testing</Text>
+          <Text className={''}></Text>
+      </SafeAreaView>
+  );
+}
+
+export default HomeScreen;
